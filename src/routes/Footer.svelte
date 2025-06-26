c00e51b0cc0c597498b68c55e5506e7c7aff99a3
gepardoj <anna.ra.phoenix@gmail.com>
2025-06-26 20:36:29 +0300

    improved UI of bottom navigation

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,18 +1,40 @@
 <script lang="ts">
   import { page } from "$app/state";
+  import eiffelTower from "$lib/images/eiffel-tower.svg?raw";
+  import profile from "$lib/images/profile.svg?raw";
+  import options from "$lib/images/options.svg?raw";
+  import Icon from "../stories/Icon.svelte";
 </script>
 
 <footer>
-  <nav>
-    <ul class="flex gap-3 place-self-center">
+  <nav class="mb-2">
+    <ul class="flex gap-6 place-self-center">
       <li aria-current={page.url.pathname === "/" ? "page" : undefined}>
-        <a href="/">Home</a>
+        <a href="/" aria-label="learn french"
+          ><Icon
+            size="large"
+            variant={page.url.pathname === "/" ? "primary" : undefined}
+            icon={eiffelTower}
+          /></a
+        >
       </li>
       <li aria-current={page.url.pathname === "/me" ? "page" : undefined}>
-        <a href="/me">Me</a>
+        <a href="/me" aria-label="my profile"
+          ><Icon
+            size="large"
+            variant={page.url.pathname === "/me" ? "primary" : undefined}
+            icon={profile}
+          /></a
+        >
       </li>
       <li aria-current={page.url.pathname === "/options" ? "page" : undefined}>
-        <a href="/options">Options</a>
+        <a href="/options" aria-label="options"
+          ><Icon
+            size="large"
+            variant={page.url.pathname === "/options" ? "primary" : undefined}
+            icon={options}
+          /></a
+        >
       </li>
     </ul>
   </nav>
---
+++ src/stories/Icon.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  interface Props {
+    variant?: "primary" | "success" | "error";
+    disabled?: boolean;
+    /** How large should the button be? */
+    size?: "small" | "medium" | "large";
+    /** Button contents */
+    icon: string;
+  }
+
+  const { variant, disabled, size = "medium", icon }: Props = $props();
+  function getClassName() {
+    let className = "stroke-svg fill-svg";
+    switch (variant) {
+      case "primary":
+        className = "stroke-primary fill-primary";
+        break;
+      case "success":
+        className = "stroke-success fill-success";
+        break;
+      case "error":
+        className = "stroke-error fill-error";
+        break;
+    }
+    if (disabled) className = "stroke-disabled fill-disabled";
+    return className;
+  }
+</script>
+
+<svg
+  class={[
+    size === "small" ? "w-4 h-4" : "",
+    size === "medium" ? "w-6 h-6" : "",
+    size === "large" ? "w-8 h-8" : "",
+    getClassName(),
+  ]}>{@html icon}</svg
+>
